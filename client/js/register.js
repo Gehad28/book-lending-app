582b41e3c651d133bed3cb34e066f3e2cb8eef72
جهاد أحمد <gehad0ahmed28@gmail.com>
2025-01-27 17:24:32 +0200

    Register Page

---
+++ client/js/register.js
@@ -0,0 +1,23 @@
+const registerForm = document.getElementById("registerForm");
+
+const register = (formData) => {
+    fetch("http://127.0.0.1:5000/user/register", {
+        method: 'POST',
+        body: formData
+    }).then(request => {
+        if (!request.ok) {
+            console.log("Something went wrong!");
+            return null;
+        }
+        return request.json();
+    }).then(data => {
+        console.log(data);
+        window.location.href = "../pages/index.html";
+    });
+}
+
+registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let formData = new FormData(registerForm);
+    register(formData);
+});
