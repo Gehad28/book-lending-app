5d31f59381dec6cca6f5c18321dddb340ae4219d
جهاد أحمد <gehad0ahmed28@gmail.com>
2025-01-27 13:39:10 +0200

    Editing PasswordField

--- server/forms.py
+++ server/forms.py
@@ -19,7 +19,7 @@ class RegisterForm(FlaskForm):
     f_name = StringField('f_name', validators=[DataRequired(message)])
     l_name = StringField('l_name', validators=[DataRequired(message)])
     email = StringField('email', validators=[DataRequired(message), Email()])
-    password = StringField('password', validators=[DataRequired(message), Length(min=6, message="Password must be at least 6 characters")])
+    password = PasswordField('password', validators=[DataRequired(message), Length(min=6, message="Password must be at least 6 characters")])
     phone = StringField('phone', validators=[DataRequired(message), 
                                             Length(min=11, max=11, message="Phone number must be 11 digits."),
                                             Regexp(r'^\+?2?\d{11}$', message="Invalid phone number.")])
@@ -41,5 +41,5 @@ class LoginForm(FlaskForm):
         
     message = "Incorrect email or password"
     email = StringField('email', validators=[DataRequired(message), Email()])
-    password = StringField('password', validators=[DataRequired(message)])
+    password = PasswordField('password', validators=[DataRequired(message)])
     submit = SubmitField('Submit')
