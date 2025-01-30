8cb0f1452e3dc8fae9e4a14f91d770852b4dd8cc
جهاد أحمد <gehad0ahmed28@gmail.com>
2025-01-30 16:11:59 +0200

    Retreiving books in the home page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from server import create_app
+from flask import send_from_directory
 
 app = create_app()
+
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    return send_from_directory("images", filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- client/js/home.js
+++ client/js/home.js
@@ -19,3 +19,59 @@ bookForm.addEventListener("submit", (e) => {
     .then(error => console.log(error));
     bookForm.reset();
 });
+
+
+// ________________________________________
+
+const booksList = document.getElementById("books-list");
+
+const addBookItems = (books) => {
+    books.forEach((book, index) => {
+        const bookElement = document.createElement("li");
+        bookElement.id = index;
+        bookElement.classList.add("book-item");
+
+        const title = document.createElement("p");
+        title.innerText = book.title;
+
+        const author = document.createElement("p");
+        author.innerText = book.author;
+
+        const container = document.createElement("div");
+        container.classList.add("info");
+        container.appendChild(title);
+        container.appendChild(author);
+
+        const image = document.createElement("img");
+        image.src = `http://127.0.0.1:5000/${book.image_path}`;
+
+        const borrowBtn = document.createElement("button");
+        borrowBtn.innerText = "Borrow";
+        borrowBtn.classList.add("borrow-btn");
+
+        bookElement.appendChild(image);
+        bookElement.appendChild(container);
+        bookElement.appendChild(borrowBtn);
+
+        booksList.appendChild(bookElement);
+    });
+}
+
+const getBooks = () => {
+    fetch("http://127.0.0.1:5000/book/get-all-books", {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        addBookItems(data.books);
+    })
+    .then(error => console.log(error));
+}
+
+window.onload = () => {
+    getBooks();
+}
