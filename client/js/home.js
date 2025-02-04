0bde805e7a55bea4152cde6744967ee0cfce94dc
جهاد أحمد <gehad0ahmed28@gmail.com>
2025-02-04 13:02:45 +0200

    Refactoring JS files: home page - delete comments

--- client/js/home.js
+++ client/js/home.js
@@ -7,44 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const bookForm = document.getElementById("bookForm");
     const message = document.getElementById("message");
-    const booksList = document.getElementById("books-list");
-
     bookForm.addEventListener("submit", (e) => {
         e.preventDefault();
         const formData = new FormData(bookForm);
         addBook(formData, function(data) { message.innerText = data.message; })
         bookForm.reset();
     });
-
-    // const addBookItems = (data) => {
-    //     data.books.forEach((book, index) => {
-    //         const bookElement = document.createElement("li");
-    //         bookElement.id = index;
-    //         bookElement.classList.add("book-item");
-    
-    //         const title = document.createElement("p");
-    //         title.innerText = book.title;
-    
-    //         const author = document.createElement("p");
-    //         author.innerText = book.author;
-    
-    //         const container = document.createElement("div");
-    //         container.classList.add("info");
-    //         container.appendChild(title);
-    //         container.appendChild(author);
-    
-    //         const image = document.createElement("img");
-    //         image.src = `http://127.0.0.1:5000/${book.image_path}`;
-    
-    //         bookElement.appendChild(image);
-    //         bookElement.appendChild(container);
-    //         const borrowBtn = createBtn("Borrow", "borrow-btn", "borrow-btn");
-    //         borrowBtn.addEventListener("click", () => borrowBook(book));
-    //         bookElement.appendChild(borrowBtn);
-    //         booksList.appendChild(bookElement);
-    //     });
-    // }
-    
 });
 
 
